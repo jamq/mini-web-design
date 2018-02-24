9acce4e7b7f3622c8e5ffe7f8a00e121d08d7ebd
Joaquín <moreno.quinteros@outlook.com>
2018-02-24 15:09:28 -0300

    V0.1

---
+++ js/scroll.js
@@ -0,0 +1,25 @@
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+$(window).scroll(function() {
+		var scrollDistance = $(window).scrollTop();
+
+		// Show/hide menu on scroll
+		//if (scrollDistance >= 850) {
+		//		$('nav').fadeIn("fast");
+		//} else {
+		//		$('nav').fadeOut("fast");
+		//}
+	
+		// Assign active class to nav links while scolling
+		$('.pg').each(function(i) {
+				if ($(this).position().top <= scrollDistance) {
+						$('.menu a.activo').removeClass('activo');
+						$('.menu a').eq(i).addClass('activo');
+				}
+		});
+}).scroll();
